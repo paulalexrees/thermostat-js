b8ec431610ce0c76f4f515ae174440e91d6f375e
phoebe <phoebe.greig@gmail.com>
2016-03-30 17:12:01 +0100

    css and interface using jquery

--- interface.js
+++ interface.js
@@ -1,14 +1,38 @@
 $( document ).ready(function(){
 
   var thermostat = new Thermostat();
-  $("#temp").text("Current temperature: " + thermostat.temp);
-  console.log("shit");
+  function updateTemp(){
+  	$("#temp").text("Current temperature: " + thermostat.temp);
+  	$('#temp').attr('class', thermostat.displayColor());
+  };
 
+  updateTemp();
+  $("input[type='checkbox']").prop('checked', true);
 
-  $("input[type='submit']").click(function(event){
+  $("input[value='Increase Temp']").click(function(event){
     thermostat.upButton();
-    $("#temp").text("Current temperature: " + thermostat.temp);
-    console.log("poo");
+    updateTemp();
   });
+
+  $("input[value='Decrease Temp']").click(function(event){
+    thermostat.downButton();
+    updateTemp();
+  });
+
+  $("input[value='Reset Temp']").click(function(event){
+    thermostat.resetButton();
+    updateTemp();
+  });
+
+  $("input[type='checkbox']").change(function(){
+    if (this.checked) {
+      thermostat.powerSavingOn();
+    } else {
+    	thermostat.powerSavingOff();    
+    };
+    updateTemp();
+	}); 
+
+
 
 });
