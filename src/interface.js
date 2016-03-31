336d64a627dc3941e89a7f770558fdea3e35c432
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-31 10:44:11 +0100

    Weather API working

--- src/interface.js
+++ src/interface.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
   var thermostat = new Thermostat;
   updateDisplay();
-function updateDisplay() {
-  $('#temperature').text(thermostat.getCurrentTemperature());
-  $('#temperature').attr('class', thermostat.temperatureColour());
-};
+
+  function updateDisplay() {
+    $('#temperature').text(thermostat.getCurrentTemperature());
+    $('#temperature').attr('class', thermostat.temperatureColour());
+  };
 
   $('#increaseTemperature').on('click', function() {
     thermostat.increaseTemperature();
@@ -25,6 +26,14 @@ function updateDisplay() {
   $('#resetTemperature').on('click', function() {
     thermostat.resetTemperature();
     updateDisplay();
+  });
+
+  $("#submit").click(function() {
+    var city = $("#city").val();
+    $("#display-city").text(city);
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c74750056623d5bf9582e07bc0b923ea&units=metric", function(data) {
+      $("#api-temp").text(data.main.temp);
+    });
   });
 
 
